1826ddc5f34a91a296a85b5c7afcd31307b8310d
AlexseiT <alex.todorov@mail.ru>
2025-04-24 23:55:13 +0700

    add

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,4 +1,8 @@
 import { signIn, signOut } from 'next-auth/react';
 
-export const handleSignIn = () => signIn('google');
+export const handleSignIn = () => 
+{
+    console.log(123);
+    //signIn('google');
+}
 export const handleSignOut = () => signOut();
