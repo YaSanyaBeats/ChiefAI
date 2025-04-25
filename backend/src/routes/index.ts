20b320fb3a1be4430b7a8b57d4050f572d6d46da
YaSanyaBeats <alexandr191261@yandex.ru>
2025-04-26 04:33:17 +0700

    Keep calm and commit

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -8,6 +8,7 @@ router.get('/', async function(req: Request, res: Response, next: NextFunction) 
   const collection = db.collection('test');
   const result = await collection.insertOne({ name: 'John', age: 30, date: Date.now() });
 
+  collection.find({'date': Date.now() - 5}).sort({'name': -1}).limit(5)
   res.send('Inserted document with ID:' + result.insertedId);
 });
 
